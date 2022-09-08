a8c631c16841743a1485474d27580f04dd629a0f
Nick M <mully7773@gmail.com>
2022-09-08 13:16:59 -0500

    Successfully fetched data from api and cloned recipe object with changed camelCase keys

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,33 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    console.log(res, data);
+    // Destructuring recipe out of the data object so...
+    let { recipe } = data.data;
+    // ...we make a copy of the object and rename from snake_case to camelCase.
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
