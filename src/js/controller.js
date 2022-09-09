215d1abe47251757cf1c9ce51407d0ad520cd5c7
Nick M <mully7773@gmail.com>
2022-09-09 10:55:01 -0500

    Began implementing MVC architecture - successfully rendered recipe using new architecture

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,5 @@
+import * as model from './model.js';
+
 // import icons from '../img/icons.svg'; //Parcel 1
 // Importing icons because the JS icons contain the old file path whereas Parcel uses a different file path in the dist folder
 import icons from 'url:../img/icons.svg'; //Parcel 2
@@ -38,31 +40,13 @@ const showRecipe = async function () {
     console.log(id);
 
     if (!id) return;
+    renderSpinner(recipeContainer);
 
     // Loading recipe
-    renderSpinner(recipeContainer);
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-      // `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcb34`
-    );
-    const data = await res.json();
+    // async function returns a promise that we need to handle whenever we call that async function
+    await model.loadRecipe(id);
+    const { recipe } = model.state;
 
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-    console.log(res, data);
-    // Destructuring recipe out of the data object so...
-    let { recipe } = data.data;
-    // ...we make a copy of the object and rename from snake_case to camelCase.
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
-    console.log(recipe);
     // Rendering recipe
     const markup = `
     <figure class="recipe__fig">
---
+++ src/js/model.js
@@ -0,0 +1,31 @@
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    console.log(res, data);
+    // Destructuring recipe out of the data object so...
+    const { recipe } = data.data;
+    // ...we make a copy of the object and rename from snake_case to camelCase.
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
