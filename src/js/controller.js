9a92a8b17e01c9591149aa6d6bab87aa6d813ef4
Nick M <mully7773@gmail.com>
2022-09-12 14:28:44 -0500

    Added controller portion of logic associated with search functionality

--- src/js/controller.js
+++ src/js/controller.js
@@ -37,6 +37,16 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    await model.loadSearchResults('pizza');
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+controlSearchResults();
+
 // controlRecipes();
 
 // window.addEventListener('hashchange', controlRecipes);
