e7e0267dd65c5add3fbcd6e63d39a982eeba988f
Nick M <mully7773@gmail.com>
2022-09-21 11:37:13 -0500

    Rendered bookmarks to bookmark tab

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 // Polyfilling everything else
 import 'core-js/stable';
@@ -34,6 +35,9 @@ const controlRecipes = async function () {
 
     // Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+
+    // So that the selected bookmark remains highlighted
+    bookmarksView.update(model.state.bookmarks);
 
     // Loading recipe
     // async function returns a promise that we need to handle whenever we call that async function
@@ -92,13 +96,18 @@ const controlServings = function (newServings) {
 };
 
 const controlAddBookmark = function () {
+  // Add/remove bookmark
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else {
     model.deleteBookmark(model.state.recipe.id);
   }
 
-  console.log(model.state.recipe);
+  // Update recipeView
+  // console.log(model.state.recipe);
   recipeView.update(model.state.recipe);
+
+  // Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
 };
 
 // Publisher/Subscriber Pattern
