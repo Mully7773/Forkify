b7350c55753073a6ef0d6590fc923e0a78cf0049
Nick M <mully7773@gmail.com>
2022-09-22 12:11:48 -0500

    Implemented add recipe view with spinner, success message, and setTimeout

--- src/js/config.js
+++ src/js/config.js
@@ -3,3 +3,4 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = '22a9e96a-fdb0-4b82-8f2d-db6320295004';
+export const MODAL_CLOSE_SEC = 2.5;
